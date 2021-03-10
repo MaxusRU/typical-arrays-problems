38d4809da99145418e69f3572b4de29db95e13ac
Maxim <kiber987@gmail.com>
2021-03-10 05:54:47 +0300

    feat: add comments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 
 exports.min = function min (array) {
   if ((array === undefined) || (array.length == 0))
+//Аргумент, который не был передан, приходит как неопределенный.
+//Тестирование со строгим равенством против нуля не удастся.
+//Вы должны использовать строгое равенство с неопределенным.
 	  return 0;
   else
 	  return array.sort((a, b) => a - b)[0];
+//https://learn.javascript.ru/array-methods
+//https://learn.javascript.ru/arrow-functions-basics
 }
 
 exports.max = function max (array) {
